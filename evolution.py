5bd3a60b80a3b6ae1871c06945f681dce1c7fa44
Piotr Kramek <peterkramek@gmail.com>
2020-10-29 14:15:08 +0100

    Add main project file

---
+++ evolution.py
@@ -0,0 +1,15 @@
+class Evolutionary:
+    def __init__(self):
+        pass
+
+
+def objective_function():
+    pass
+
+def encode_gray():
+    pass
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+
